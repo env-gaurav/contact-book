d9112ff03ec8dd8ee1e9dd6eb5524e2a7d28932e
env-gaurav <gaurav2528singh@gmail.com>
2025-02-04 23:35:52 +0530

    contact book

---
+++ main.py
@@ -0,0 +1,18 @@
+def display_msg():
+    msg='''
+     CONTACT BOOK
+     ------------
+    1. Add Contact
+    2. Search Contact
+    3. Update Contact
+    4. Delete Contact
+    5. Display Contact
+    6. Exit 
+'''
+    return print(msg)
+    
+def main():
+    display_msg()
+
+if __name__ == "__main__":
+    main()
