a0c54921af68a0a62ae48b1f991a5a1e990295e5
env-gaurav <gaurav2528singh@gmail.com>
2025-02-05 23:02:43 +0530

    new features added: search contact, update contact, display contact, add contact

--- main.py
+++ main.py
@@ -17,27 +17,46 @@ def add_contact(contacts):
     email=input("Enter Email: ")
     address=input("Enter Address: ")
     contacts[name]={"Phone":phone, "Email":email, "Address":address}
-    print("success")
+    print("Record added successfully")
+
+def display_contacts(contacts):
+    print(contacts)
+
+def update_contact(contacts):
+    name= input("Enter Name: ")
+    phone=input("Enter Phone: ")
+    email=input("Enter Email: ")
+    address=input("Enter Address: ")
+    contacts[name]={"Phone":phone, "Email":email, "Address":address}
+    print("Record updated successfully")
+
+def search_contact(contacts):
+    name= input("Enter Name: ")
+    contact = contacts.get(name,"Not found")
+    print(contact)
 
 def main():
-    contacts={}
+    contacts={
+        'Alice': {'phone': '123-456-7890', 'email': 'alice@example.com', 'address': '123 Maple Street'},
+    'Bob': {'phone': '987-654-3210', 'email': 'bob@example.com', 'address': '456 Oak Avenue'},
+    'Charlie': {'phone': '555-555-5555', 'email': 'charlie@example.com', 'address': '789 Pine Road'}
+    }
     display_msg()
     while True:
         choice=int(input("Enter Choice: "))
         if choice == 6:
-            print(contacts)
             print("Exit")
             break
         elif choice == 1:
             add_contact(contacts)
         elif choice == 2:
-            pass
+            search_contact(contacts)
         elif choice == 3:
-            pass
+            update_contact(contacts)
         elif choice == 4:
             pass
         elif choice == 5:
-            pass
+            display_contacts(contacts)
         else:
             print("Invalid choice")
 
